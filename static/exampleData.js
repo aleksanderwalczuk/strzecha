c5b5eed7403f04744b321687038e0014812c9c48
aleksanderwalczuk <ohleg@windowslive.com>
2022-04-03 10:35:38 +0200

    fix: fix news images

--- static/exampleData.js
+++ static/exampleData.js
@@ -2,23 +2,23 @@
 const data = [
   {
     id: 1,
-    img: require('./images/product-1.jpg'),
-    imgLg: require('./images/product-1@2x.jpg'),
+    img: '/images/product-1.jpg',
+    imgLg: '/images/product-1@2x.jpg',
     link: '',
     title: 'Filiżanka kolekcjonerska Rosenthal',
   },
   {
     id: 2,
-    img: require('./images/product-2.jpg'),
-    imgLg: require('./images/product-2@2x.jpg'),
+    img: '/images/product-2.jpg',
+    imgLg: '/images/product-2@2x.jpg',
     link: '',
     title: 'Maryja Immaculata',
 
   },
   {
     id: 3,
-    img: require('./images/product-3.jpg'),
-    imgLg: require('./images/product-3@2x.jpg'),
+    img: '/images/product-3.jpg',
+    imgLg: '/images/product-3@2x.jpg',
     link: '',
     title: 'Fotel Sztuka Chin',
   },
