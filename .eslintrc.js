eb707db90a0e9bb23581e9d9f091ed1e40e57c49
aleksanderwalczuk <ohleg@windowslive.com>
2023-01-22 09:23:15 +0100

    chore: add base eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,36 @@ module.exports = {
   root: true,
   extends: [
     "plugin:vue/recommended",
-    // "standard-with-typescript"
+    "standard-with-typescript"
 
   ],
   overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./src/**.vue"]
+      },
+      rules: {
+        "space-before-function-paren": 0,
+        "@typescript-eslint/return-await": 0,
+        "no-return-await": "error"
+      }
+    }
   ],
   parser: "vue-eslint-parser",
-
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    ecmaVersion: "latest",
-    sourceType: "module",
-    // project: ["./tsconfig.json", "./src/**.vue"]
-  },
   plugins: [
     "vue"
   ],
   rules: {
     semi: 0,
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/space-before-function-paren": 0,
     "space-before-function-paren": 0,
     quotes: [2, "double", { avoidEscape: true, allowTemplateLiterals: true }],
-    // "@typescript-eslint/quotes": [2, "double"],
+    "@typescript-eslint/quotes": [2, "double"]
     // "@typescript-eslint/semi": [2, "always"]
   }
-}
+};
