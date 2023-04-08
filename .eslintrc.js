bf25fe2eed69188793647aa2f976c81b7c09ca85
aleksanderwalczuk <ohleg@windowslive.com>
2023-04-08 12:28:00 +0200

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,76 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  root: true,
   extends: [
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
     "plugin:vue/recommended",
-    "standard-with-typescript"
-
-  ],
-  overrides: [
-    {
-      files: ["*.ts"],
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: ["./tsconfig.json", "./src/**.vue"]
-      },
-      rules: {
-        "space-before-function-paren": 0,
-        "@typescript-eslint/return-await": 0,
-        "no-return-await": "error"
-      },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
-      ]
-    },
-    {
-      files: ["*.vue"],
-      extends: "plugin:vue/recommended",
-      rules: {
-        "vue/multi-word-component-names": 0
-      }
-    }
   ],
   parser: "vue-eslint-parser",
   parserOptions: {
-    parser: {
-      ts: "@typescript-eslint/parser",
-      "<template>": "espree"
-    }
+    parser: "@typescript-eslint/parser",
+    extraFileExtensions: [".vue"],
   },
-  plugins: [
-    "vue"
-  ],
   rules: {
-    semi: 0,
-    "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/space-before-function-paren": 0,
-    "space-before-function-paren": 0,
-    quotes: [2, "double", { avoidEscape: true, allowTemplateLiterals: true }],
-    "@typescript-eslint/quotes": [2, "double"],
-    "vue/multi-word-component-names": 0
-    // "@typescript-eslint/semi": [2, "always"]
-  }
+    semi: ['error', 'always'],
+    'es/no-rest-spread-properties': 'off',
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'never',
+    }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      ts: 'never',
+    }],
+    '@typescript-eslint/type-annotation-spacing': ['error'],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: ['**/*.test.{ts,js}'],
+    }],
+    '@typescript-eslint/strict-boolean-expressions': ['error', {
+      allowNumber: false,
+    }],
+    '@typescript-eslint/ban-types': ['error', {
+      extendDefaults: true,
+      types: {
+        'Record<string, string>': {
+          message: 'Use Record<string, string|undefined> instead',
+          fixWith: 'Record<string, string|undefined>',
+        },
+        'Record<string, number>': {
+          message: 'Use Record<string, number|undefined> instead',
+          fixWith: 'Record<string, number|undefined>',
+        },
+      },
+    }],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+  env: {
+    browser: true,
+  },
+  plugins: ["@typescript-eslint", "import"],
+  overrides: [
+    {
+      files: ["tailwind.config.js"],
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
+  settings: {
+    polyfills: [],
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ["tsconfig.json", "packages/*/tsconfig.json"],
+      },
+    },
+  },
+  ignorePatterns: [".nuxt/**", "node_modules/"],
 };
